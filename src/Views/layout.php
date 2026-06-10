25d24465b2fbc49a953c7f7425016287c470181b
PC Uninta - tarde <caiomm076@gmail.com>
2026-06-10 17:58:38 -0300

    feat: adaptar página para receber o header sem perder a posição da main e do footer

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
+    <?php require_once __DIR__ . '/partials/header.php'; ?>
+
     <?php include __DIR__ . '/partials/aside.php'; ?>
 
     <div class="conteudo-central">
---
+++ src/Views/partials/header.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="texto-cabecalho">
+        <h1>Gerenciador Financeiro</h1>
+        <p>Organize suas finanças de forma simples e eficiente</p>
+    </div>
+</header>
