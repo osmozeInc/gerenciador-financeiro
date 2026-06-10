e8fe250b3bfd3e4569f8a457072032a60bc29bd0
PC Uninta - tarde <caiomm076@gmail.com>
2026-06-10 16:28:30 -0300

    feat: cria a estrutura visual e css do menu lateral (aside)

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Gerenciador Financeiro</title>
-
     <link rel="stylesheet" href="/assets/css/layout.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
 
-    <aside class="menu-lateral">
-        <?php include __DIR__ . '/partials/aside.php'; ?>
-    </aside>
+    <?php include __DIR__ . '/partials/aside.php'; ?>
 
     <main class="conteudo-central">
         <?php 
@@ -19,10 +17,7 @@
         ?>
     </main>
 
-    <footer class="rodape">
-        <?php require_once __DIR__ . '/partials/footer.php'; ?>
-    </footer>
-
+    <?php require_once __DIR__ . '/partials/footer.php'; ?>
 
 </body>
 </html>
--- src/Views/partials/aside.php
+++ src/Views/partials/aside.php
@@ -1,4 +1,63 @@
-<div>
-    <h2>Aside</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
-</div>
+<?php
+$urlAtual = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+?>
+
+<aside class="aside-container">
+    <div class="logo-container">
+        <img src="/images/marca/logo_escura.png" alt="Logo do Gerenciador Financeiro">
+        <h2>Pro Gestão</h2>
+    </div>
+    
+    
+    <div class="menus-container">
+        <div class="menu-container">
+            <hr class="logo-divider">
+            <h2>Menu</h2>
+            <a href="/home" class="menu-item <?= $urlAtual === '/home' ? 'active' : ''; ?> <?= $urlAtual === '/' ? 'active' : ''; ?>">
+                <i class="bi bi-clipboard"></i>
+                <p class="menu-item-text">Visão Geral</p>
+                <i class="bi bi-chevron-right"></i>
+            </a>
+            <a href="/transacoes" class="menu-item <?= $urlAtual === '/transacoes' ? 'active' : ''; ?>">
+                <i class="bi bi-wallet"></i>
+                <p class="menu-item-text">Transações</p>
+                <i class="bi bi-chevron-right"></i>
+            </a>
+            <a href="/investimentos" class="menu-item <?= $urlAtual === '/investimentos' ? 'active' : ''; ?>">
+                <i class="bi bi-graph-up"></i>
+                <p class="menu-item-text">Investimentos</p>
+                <i class="bi bi-chevron-right"></i>
+            </a>
+            <a href="/cofres" class="menu-item <?= $urlAtual === '/cofres' ? 'active' : ''; ?>">
+                <i class="bi bi-piggy-bank"></i>
+                <p class="menu-item-text">Cofres</p>
+                <i class="bi bi-chevron-right"></i>
+            </a>
+            <a href="/contas" class="menu-item <?= $urlAtual === '/contas' ? 'active' : ''; ?>">
+                <i class="bi bi-credit-card"></i>
+                <p class="menu-item-text">Contas e Cartões</p>
+                <i class="bi bi-chevron-right"></i>
+            </a>
+        </div>
+    
+        <div class="submenu-container">
+            <hr class="logo-divider">
+            <a href="" class="submenu-item">
+                <i class="bi bi-bell"></i>
+                <p class="submenu-item-text">Notificações</p>
+            </a>
+            <a href="" class="submenu-item">
+                <i class="bi bi-gear"></i>
+                <p class="submenu-item-text">Configurações</p>
+            </a>
+            <button class="user-button">
+                <i class="bi bi-person-fill"></i>
+                <div class="user-text-container">
+                    <p class="user-name">Caio Monte</p>
+                    <p class="user-email">caio.monte@example.com</p>
+                </div>
+            </button>
+        </div>
+    </div>
+
+</aside>
