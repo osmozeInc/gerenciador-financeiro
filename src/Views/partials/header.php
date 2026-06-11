86dde3207443bdfb42b2125c0258f7c905bf3050
PC Casa <caiomm076@gmail.com>
2026-06-10 23:45:08 -0300

    style: personaliza header

--- src/Views/partials/header.php
+++ src/Views/partials/header.php
@@ -1,6 +1,15 @@
 <header>
+    <hr>
+    
     <div class="texto-cabecalho">
         <h1>Gerenciador Financeiro</h1>
-        <p>Organize suas finanças de forma simples e eficiente</p>
+        <p>Visão Geral - 10/06/2026</p>
     </div>
+
+    <nav>
+        <a class="active">Financeiro</a>
+        <a>Atividades</a>
+        <a>Outro</a>
+        <a>Empresa</a>
+    </nav>
 </header>
